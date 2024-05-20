c9ce05cf5bef0de2516d3ff02cc22e7a63014ad3
muskan1024 <shaikhmuskan1024@gmail.com>
2024-05-21 00:17:16 +0530

    home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Shop from './Shop'
 import Navbar from './Navbar'
 
 const Home = () => {
   return (
     <>
         <Navbar/>
+        <div className='grid '>
+            <img src="/images/baner-1.png" alt="baner" className='w-[90%] rounded-lg lg:max-w-[70%] mt-5 justify-self-center' />
+        </div>
     </>
   )
 }
