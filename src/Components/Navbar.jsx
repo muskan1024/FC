e4691cbe0527c8c53839c048c42aae191b18e4f3
NazarShaikh <shaikhnazar265@gmail.com>
2024-05-20 14:33:43 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import { AccountCircle, Search, ShoppingCart } from "@mui/icons-material";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between max-w-[90%] bg-gray-50">
-      <img src="/images/fc-logo.png" alt="" />
-      <div className="search">
-        <input type="text" placeholder='Search..' />
-
+    <div className="flex justify-between  bg-gray-400 py-4 px-5 shadow-lg">
+      <div className="flex items-center gap-2 font-bold text-xl">
+        <img src="/images/fc-logo.png" alt="" className="w-5 " />
+        <span className="text-red-600 ">FASHION</span>
+        <span>COLLECTION</span>
       </div>
-      <ul className="flex gap-4">
+
+      <div className="bg-white border-b-[1.5px] border-black px-1 ">
+        <input type="text" placeholder="Search.." className="outline-none " />
+        <Search />
+      </div>
+      <ul className="flex gap-5 text-lg ">
         <a href="#home">Home</a>
         <a href="#shop">Shop</a>
-        <button>Login</button>
+        <div>
+          <AccountCircle className="mr-1" />
+          <button>Login</button>
+        </div>
+       <a href=""><ShoppingCart /> </a> 
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
