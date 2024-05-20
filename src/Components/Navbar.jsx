f769a14fcfe80706b781ace4d77109b41d09787f
NazarShaikh <shaikhnazar265@gmail.com>
2024-05-20 18:26:09 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,29 +1,56 @@
-import { AccountCircle, Search, ShoppingCart } from "@mui/icons-material";
-import React from "react";
+import { AccountCircle, Menu, Search, ShoppingCart } from "@mui/icons-material";
+import { Hidden } from "@mui/material";
+import React, { useState } from "react";
 
 const Navbar = () => {
-  return (
-    <div className="flex justify-between  bg-gray-400 py-4 px-5 shadow-lg">
-      <div className="flex items-center gap-2 font-bold text-xl">
-        <img src="/images/fc-logo.png" alt="" className="w-5 " />
-        <span className="text-red-600 ">FASHION</span>
-        <span>COLLECTION</span>
-      </div>
+  const [menu, setMenu] = useState("home");
+  const [open, setOpen] = useState(false);
 
-      <div className="bg-white border-b-[1.5px] border-black px-1 ">
-        <input type="text" placeholder="Search.." className="outline-none " />
-        <Search />
-      </div>
-      <ul className="flex gap-5 text-lg ">
-        <a href="#home">Home</a>
-        <a href="#shop">Shop</a>
-        <div>
-          <AccountCircle className="mr-1" />
-          <button>Login</button>
+  return (
+    <>
+      <div>
+        <div className="grid grid-cols-2 md:grid-cols-3 justify-items-center md:justify-around px-2 py-3 shadow-lg">
+          <div className="justify-self-center w-full md:w-fit flex items-center gap-2 font-bold text-xl">
+            <img src="/images/fc-logo.png" alt="" className="w-5 " />
+            <span className="text-red-600 ">FASHION</span>
+            <span>COLLECTION</span>
+          </div>
+          <div
+            className="justify-self-end md:hidden block"
+            onClick={() => setOpen(!open)}
+          >
+            <Menu />
+          </div>
+          <div
+            className={`${
+              open ? "hidden" : "block"
+            } w-full grid grid-flow-row md:grid-flow-col gap-4 col-span-2 justify-evenly`}
+          >
+            {/* Search Bar */}
+            <div className="flex border-b-[1.5px] border-black bg-white">
+              <input
+                type="text"
+                placeholder="Search.."
+                className="outline-none "
+              />
+              <Search />
+            </div>
+            {/* Menu Bar */}
+            <ul className="flex gap-5 text-sm md:text-lg ">
+              <a href="#home">Home</a>
+              <a href="#shop">Shop</a>
+              <div>
+                <AccountCircle className="mr-1" />
+                <button>Login</button>
+              </div>
+              <a href="">
+                <ShoppingCart />
+              </a>
+            </ul>
+          </div>
         </div>
-       <a href=""><ShoppingCart /> </a> 
-      </ul>
-    </div>
+      </div>
+    </>
   );
 };
 
